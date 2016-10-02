addf8d8fd894f9aa9de42badc149cf45347385eb
dovgreenwood <dov.greenwood@gmail.com>
2016-10-02 11:15:01 -0400

    Uploaded program file

---
+++ Joust_Controller/Joust_Controller.ino
@@ -0,0 +1,17 @@
+
+void setup() {
+  pinMode(4, INPUT); //button
+  pinMode(8, INPUT); //right
+  pinMode(12, INPUT); //left
+  Keyboard.begin();
+}
+
+void loop() {
+  if(digitalRead(8))
+    Keyboard.write('d');
+  if(digitalRead(12))
+    Keyboard.write('a');
+  if(digitalRead(4))
+    Keyboard.write('l');
+  delayMicroseconds(1000);
+}
